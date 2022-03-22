f57aae590284d214e994a6f0e2d9bf31e3f17320
willmexe <imdark200@gmail.com>
2022-03-22 22:17:54 +0100

    Inital gh-pages

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/willmexe/carlssonswordle.git', // Update to point to your repository  
+        repo: 'https://github.com/willmexe/SotarksWordle.git', // Update to point to your repository  
         user: {
             name: 'willmexe', // update to use your name
             email: 'imdark200@gmail.com' // Update to use your email
